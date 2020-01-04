3219cc5e860f5898770ababc9d851981fa0100ef
DARK <darksoul2407@gmail.com>
2020-01-04 18:29:58 +0400

    Добавьте файлы проекта.

---
+++ testBluetooth/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace testBluetooth
+{
+    public partial class Form1 : Form
+    {
+        int count = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            serialPort1.PortName = "COM8";
+            serialPort1.BaudRate = 9600;
+            try
+            {
+                if (!serialPort1.IsOpen)
+                    serialPort1.Open();
+
+            }
+            catch (Exception e) {
+                MessageBox.Show("ошибка компорта");
+            }serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(DataReceived);
+        }
+            
+        private void DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            //throw new NotImplementedException();
+            try {
+                SerialPort spl = (SerialPort)sender;
+                Console.Write("Data"+count+ "  "+ spl.ReadLine()+ "\n");
+                count++;
+            } 
+            catch (Exception ew) { }
+        }
+    }
+}
